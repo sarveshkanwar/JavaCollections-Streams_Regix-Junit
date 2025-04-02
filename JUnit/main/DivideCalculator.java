335e720b1acf65a3e988132ea01554fb826e2c6d
garchomp009 <91824438+sarveshkanwar@users.noreply.github.com>
2025-04-02 11:28:50 +0530

    [uploaded]:DivideCalculator

---
+++ JUnit/main/DivideCalculator.java
@@ -0,0 +1,8 @@
+public class DivideCalculator {
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+}
